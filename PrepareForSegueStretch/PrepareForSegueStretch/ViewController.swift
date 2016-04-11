2bee88b7f6514f3c7ba58632eed58e092828406c
Vince <vincent.van.snow@gmail.com>
2016-04-11 09:35:45 -0600

    Pre Going Over

--- PrepareForSegueStretch/PrepareForSegueStretch/ViewController.swift
+++ PrepareForSegueStretch/PrepareForSegueStretch/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func firstButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func secondButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "toSecondVC" {
+            if let secondViewController = segue.destinationViewController as? SecondViewController 
+            
+            
+        } else if segue.identifier == "toThirdVC" {
+            
+            
+            
+        }
+    
 }
 
